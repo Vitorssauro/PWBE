556590310378192a98387ba548c3e987c0c25845
Vitorssauro <vitorvtr2003@gmail.com>
2023-08-03 21:56:56 -0300

    03/08/2023

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -6,18 +6,22 @@ public class ExemploThrow {
     public static void main(String[] args) {
         boolean ativo = true;
         while (ativo) {
-
-            String senhaDigitada = JOptionPane.showInputDialog("Digite uma senha");
+            String loginDigitado = JOptionPane.showInputDialog("Informe seu Login:");
+            String senhaDigitada = JOptionPane.showInputDialog("Informe sua Senha:");
 
             try {
+                if (loginDigitado.equals(senhaDigitada)) {
+                    throw new Exception("Login não pode ser igual a Senha!");
+                }
                 if (senhaDigitada.length() != 6) {
-                    throw new Exception("Senha Inválida!");
+                    throw new Exception("A senha precisa ter 6 dígitos!");
                 }
                 ativo = false;
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
             }
         }
+        JOptionPane.showMessageDialog(null,"Cradastado!!1!");
     }
 
 }
