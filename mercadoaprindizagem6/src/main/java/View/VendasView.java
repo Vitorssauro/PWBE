1004c777bca3a914613b7101caf0d6386d190230
Vitorssauro <vitorvtr2003@gmail.com>
2023-11-29 21:00:41 -0300

    29/11/2023

--- mercadoaprindizagem6/src/main/java/View/VendasView.java
+++ mercadoaprindizagem6/src/main/java/View/VendasView.java
@@ -1,37 +1,61 @@
 package View;
 
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
-
-import Connection.CadastroDAO;
-import Connection.ProdutosDAO;
-import Model.Cadastro;
-import Model.Produtos;
+import javax.swing.table.DefaultTableModel;
 
 public class VendasView extends JPanel {
-    JComboBox<String> produtosComboBox;
-    private JComboBox codBarrasBox;
     private JButton procurar, adicionar, cadVenda;
-    private JTextField cadCpfField;
+    private JTextField cadCpfField,totalField,totalDescField;
+    private DefaultTableModel tableModel;
+    private JTable table;
 
     public VendasView() {
         super();
-        
+
+        //add(new JLabel("Venda de Produto"));
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(5, 2));
-        inputPanel.add(new JLabel("Nome"));
-        cadCpfField = new JTextField(20);
+        inputPanel.setLayout(new GridLayout(2,2));
+
+        cadCpfField = new JTextField("Digite o CPF", 22);
+        inputPanel.add(cadCpfField);
+
+        inputPanel.add(procurar = new JButton("Procurar"));
+
+        // componentes Jcombobox
+        JComboBox codBarrasBox = new JComboBox<>();
+        codBarrasBox.addItem("Cod. de Barras");
+        codBarrasBox.addItem("1");
+        codBarrasBox.addItem("2");
+        inputPanel.add(codBarrasBox);
+
+        inputPanel.add(adicionar = new JButton("Adicionar"));
+
         add(inputPanel);
+
+        // tabela de compras
+        JScrollPane jSPane = new JScrollPane();
+        add(jSPane);
+        tableModel = new DefaultTableModel(new Object[][] {},
+                new String[] { "Item", "Qtd.", "$Un", "Preço Total" });
+        table = new JTable(tableModel);
+        jSPane.setViewportView(table);
+
+        JPanel submitPanel = new JPanel();
+        totalField = new JTextField("Valor total", 17);
+        submitPanel.add(totalField);
+        totalDescField = new JTextField("Total Desc.", 17);
+        submitPanel.add(totalDescField);
+
+        submitPanel.add(cadVenda = new JButton("Cad. Venda"));
+
+        add(submitPanel);
     }
-
-
 
 }
